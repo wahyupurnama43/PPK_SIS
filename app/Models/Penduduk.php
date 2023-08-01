3aa825be3c01346e9c758f086f85bd7db2ce04bb
wahyupurnama43 <wahyupurnama434@gmail.com>
2023-08-01 11:16:27 +0800

    FEAT: create migration

--- app/Models/Penduduk.php
+++ app/Models/Penduduk.php
@@ -9,4 +9,23 @@ class Penduduk extends Model
 {
     use HasFactory;
     protected $table = 'penduduk';
+    protected $hidden = ['id'];
+    protected $fillable = [
+        'nik',
+        'no_kk',
+        'no_akta_kawin',
+        'id_pekerjaan',
+        'nama_lengkap',
+        'jenis_kelamin',
+        'tempat_lahir',
+        'tanggal_lahir',
+        'agama',
+        'golongan_darah',
+        'pendidikan',
+        'status_dalam_keluarga',
+        'status_kawin',
+        'no_akta_lahir',
+        'nama_lengkap_ayah',
+        'nama_lengkap_ibu'
+    ];
 }
